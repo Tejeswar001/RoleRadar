6289edc7fe30583e5be7368ec4b013b1d67cd426
Tejeswar Naidu Sabbavarapu <148424898+Tejeswar001@users.noreply.github.com>
2025-05-24 13:10:04 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyATP4nMyzbMbC5wSHvhhd65RpW_VjoCYUo")
-
-model = genai.GenerativeModel("gemini-1.5-flash")  # or "gemini-pro"
-
-response = model.generate_content("Explain how AI works in a few words")
-
-print(response.text)
